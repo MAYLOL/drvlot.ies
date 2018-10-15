da5dc4d22534fa97cc827acfec7faaddf1c08ed0
h <h@>
2018-10-15 16:54:28 +0800

    Add DrvLot.IES & test

---
+++ ies.go
@@ -0,0 +1,35 @@
+// h 20181015
+//
+// DrvLot.IES
+
+package ies
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	. "github.com/ethereum/go-ethereum/crypto/ecies"
+)
+
+func enc(pub string, msg, tkn []byte) string {
+	ret := ""
+	for {
+		_pub, err := hex.DecodeString(pub)
+		if err != nil {
+			break
+		}
+		ctt, err := Encrypt(rand.Reader, ImportECDSAPublic(crypto.ToECDSAPub(_pub)), msg, nil, tkn)
+		if err != nil {
+			break
+		} else {
+			ret = hex.EncodeToString(ctt)
+		}
+		//
+		// Finally
+		if true {
+			break
+		}
+	}
+	return ret
+}
